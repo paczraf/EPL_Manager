0b65dc0964d1c4693b6193c970f23e6de4d5620e
Rafal Paczkowski <raf.paczkowski@gmail.com>
2020-05-12 22:01:30 +0200

    New project created - application properties updated

---
+++ src/main/java/com/paczraf/EPL_Manager/EplManagerApplication.java
@@ -0,0 +1,13 @@
+package com.paczraf.EPL_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paczraf/EPL_Manager/EplManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paczraf.EPL_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
