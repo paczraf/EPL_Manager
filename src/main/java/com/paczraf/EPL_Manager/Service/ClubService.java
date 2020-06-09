82e572a0582fa98ea3d4b9124ec7a5554c3fe68d
Rafal Paczkowski <raf.paczkowski@gmail.com>
2020-06-09 11:06:33 +0200

    ClubService added

---
+++ src/main/java/com/paczraf/EPL_Manager/Service/ClubService.java
@@ -0,0 +1,17 @@
+package com.paczraf.EPL_Manager.Service;
+
+import com.paczraf.EPL_Manager.Model.Club;
+import com.paczraf.EPL_Manager.Model.Player;
+import com.paczraf.EPL_Manager.Repository.ClubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClubService {
+
+    @Autowired
+    ClubRepository clubRepository;
+
+}
