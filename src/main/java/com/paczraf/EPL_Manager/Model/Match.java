cea6e6996b785dd76f3a5bcd3532c76443070a7e
Rafal Paczkowski <raf.paczkowski@gmail.com>
2020-06-09 11:08:04 +0200

    Match model corrected - table relationships added

--- src/main/java/com/paczraf/EPL_Manager/Model/Match.java
+++ src/main/java/com/paczraf/EPL_Manager/Model/Match.java
@@ -1,6 +1,7 @@
 package com.paczraf.EPL_Manager.Model;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import javax.persistence.Table;
@@ -18,5 +19,9 @@ public class Match {
     private Club teamNo2;
     private int teamNo1Goals;
     private int teamNo2Goals;
+    @OneToOne
+    private Tactic teamNo1Tactic;
+    @OneToOne
+    private Tactic teamNo2Tactic;
     private int result;
 }
