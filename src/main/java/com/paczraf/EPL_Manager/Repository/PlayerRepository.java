8fb8c76f70d7a7f524e0add014bb2d309f03817d
Rafal Paczkowski <raf.paczkowski@gmail.com>
2020-06-09 11:12:56 +0200

    Methods to select best players from club added

--- src/main/java/com/paczraf/EPL_Manager/Repository/PlayerRepository.java
+++ src/main/java/com/paczraf/EPL_Manager/Repository/PlayerRepository.java
@@ -1,8 +1,8 @@
 package com.paczraf.EPL_Manager.Repository;
 
-import com.paczraf.EPL_Manager.Model.Club;
 import com.paczraf.EPL_Manager.Model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +11,20 @@ import java.util.List;
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
     List<Player> getAllByClub_Id(Integer club_id);
+
+    @Query(value = "SELECT SUM(df_skills) FROM (SELECT * FROM Player  WHERE position=\"DF\" AND " +
+                    "club_id=:club_id ORDER BY df_Skills DESC limit :numberOfPlayers" +
+                    ") AS sumOfBest", nativeQuery = true)
+    int countDfSkillsByClub(int club_id, int numberOfPlayers);
+
+    @Query(value = "SELECT SUM(mf_skills) FROM (SELECT * FROM Player  WHERE position=\"MF\" AND " +
+            "club_id=:club_id ORDER BY mf_Skills DESC limit :numberOfPlayers" +
+            ") AS sumOfBest", nativeQuery = true)
+    int countMfSkillsByClub(int club_id, int numberOfPlayers);
+
+    @Query(value = "SELECT SUM(fw_skills) FROM (SELECT * FROM Player  WHERE position=\"FW\" AND " +
+            "club_id=:club_id ORDER BY fw_Skills DESC limit :numberOfPlayers" +
+            ") AS sumOfBest", nativeQuery = true)
+    int countFwSkillsByClub(int club_id, int numberOfPlayers);
+
 }
